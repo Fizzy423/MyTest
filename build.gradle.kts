70151310b4d262ba6ff992e66a8e3a39afc8d793
FizzyMoon <fizzymoon15423gmail.com>
2026-02-07 08:49:25 +0900

    первый

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Настройки плагинов для всего проекта
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+}
